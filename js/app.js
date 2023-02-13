41875138a18feade2fe430a8d77e999312006b0f
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-02-13 18:30:19 +0500

    Firs commit

---
+++ js/app.js
@@ -0,0 +1,115 @@
+function bodybgchange(params) {
+  // if (document.getElementById("body").style.backgroundColor == "#F2F2F2") {
+  //   document.getElementById("body").style.backgroundColor = "#141D2F";
+  // } else {
+  //   document.getElementById("body").style.backgroundColor = "#141D2F";
+  // }
+  // document.getElementById("boxbgchange").style.backgroundColor = "#1E2A47";
+  // document.getElementById("conrwbg").style.backgroundColor = "#141D2F";
+  // document.getElementById("boxcontentbgchange").style.backgroundColor =
+  //   "#1E2A47";
+  // document.getElementById("titleclrchange").style.color = "#FFFFFF";
+  // document.getElementById("textclrprimary").style.color = "#FFFFFF";
+  // document.getElementsByClassName("textclrprimary").style.color = "#FFFFFF";
+
+  if (
+    document.getElementById("body").className ==
+    "h-full bg-[#F2F2F2] font-mono bodybgchanlh"
+  ) {
+    document.getElementById("body").className =
+      "h-full bg-[#F2F2F2] font-mono bodybgchandk";
+  } else {
+    document.getElementById("body").className =
+      "h-full bg-[#F2F2F2] font-mono bodybgchanlh";
+  }
+  // end
+  if (
+    document.getElementById("boxbgchange").className ==
+    "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchandk"
+  ) {
+    document.getElementById("boxbgchange").className =
+      "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchanlh";
+  } else {
+    document.getElementById("boxbgchange").className =
+      "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchandk";
+  };
+  // end
+  if (
+    document.getElementById("boxcontentbgchange").className ==
+    "content-box w-full bg-[#FEFEFE] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl"
+  ) {
+    document.getElementById("boxcontentbgchange").className =
+      "content-box w-full bg-[#1E2A47] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl";
+  } else {
+    document.getElementById("boxcontentbgchange").className =
+      "content-box w-full bg-[#FEFEFE] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl";
+  };
+  // end
+  if (
+    document.getElementById("titleclrchange").className ==
+    "font-bold text-[26px] text-[#222731]"
+  ) {
+    document.getElementById("titleclrchange").className =
+      "font-bold text-[26px] text-[#FFFFFF]";
+  } else {
+    document.getElementById("titleclrchange").className =
+      "font-bold text-[26px] text-[#222731]";
+  }
+  // end
+  if (
+    document.getElementById("textclrprimary").className ==
+    "font-bold text-[26px] text-[#222731]"
+  ) {
+    document.getElementById("textclrprimary").className =
+      "font-bold text-[26px] text-[#FFFFFF]";
+  } else {
+    document.getElementById("textclrprimary").className =
+      "font-bold text-[26px] text-[#222731]";
+  }
+  // end
+  if (
+    document.getElementById("conrwbg").className ==
+    "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#F6F8FF]"
+  ) {
+    document.getElementById("conrwbg").className =
+      "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#141D2F]";
+  } else {
+    document.getElementById("conrwbg").className =
+      "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#F6F8FF]";
+  }
+  // end
+  if (
+     document.getElementById("h2-one").className ==
+    "text-[#2B3442] font-bold text-2xl"
+  ) {
+     document.getElementById("h2-one").className =
+      "text-[#FFFFFF] font-bold text-2xl";
+  } else {
+     document.getElementById("h2-one").className =
+      "text-[#2B3442] font-bold text-2xl";
+  }
+  // end
+  if (
+     document.getElementById("h2-two").className ==
+    "text-[#2B3442] font-bold text-2xl"
+  ) {
+     document.getElementById("h2-two").className =
+      "text-[#FFFFFF] font-bold text-2xl";
+  } else {
+     document.getElementById("h2-two").className =
+      "text-[#2B3442] font-bold text-2xl";
+  }
+  // end
+  if (
+     document.getElementById("h2-three").className ==
+    "text-[#2B3442] font-bold text-2xl"
+  ) {
+     document.getElementById("h2-three").className =
+      "text-[#FFFFFF] font-bold text-2xl";
+  } else {
+     document.getElementById("h2-three").className =
+      "text-[#2B3442] font-bold text-2xl";
+  }
+  // end
+  // document.getElementsByClassName("h2")
+}
