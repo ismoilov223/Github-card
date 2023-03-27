f22cd2f4f18af0646e82ac9428619beafb84d73c
ismoilov223 <shukurulloismolilov832@gmail.com>
2023-03-27 22:35:23 +0500

    Api ulandi

--- js/app.js
+++ js/app.js
@@ -1,115 +1,57 @@
-function bodybgchange(params) {
-  // if (document.getElementById("body").style.backgroundColor == "#F2F2F2") {
-  //   document.getElementById("body").style.backgroundColor = "#141D2F";
-  // } else {
-  //   document.getElementById("body").style.backgroundColor = "#141D2F";
-  // }
-  // document.getElementById("boxbgchange").style.backgroundColor = "#1E2A47";
-  // document.getElementById("conrwbg").style.backgroundColor = "#141D2F";
-  // document.getElementById("boxcontentbgchange").style.backgroundColor =
-  //   "#1E2A47";
-  // document.getElementById("titleclrchange").style.color = "#FFFFFF";
-  // document.getElementById("textclrprimary").style.color = "#FFFFFF";
-  // document.getElementsByClassName("textclrprimary").style.color = "#FFFFFF";
-
-  if (
-    document.getElementById("body").className ==
-    "h-full bg-[#F2F2F2] font-mono bodybgchanlh"
-  ) {
-    document.getElementById("body").className =
-      "h-full bg-[#F2F2F2] font-mono bodybgchandk";
+// start the Dom elements!
+const theme_change = document.querySelector("#theme_change");
+const serchInpEl = document.querySelector("#serch_inp");
+const serchBtnEl = document.querySelector("#serch_btn");
+const theme_text = document.querySelector("#theme_text");
+const theme_icon = document.querySelector("#theme_icon");
+const user_name = document.querySelector("#user_name");
+const user_date_add = document.querySelector("#user_date_add");
+const user_bio = document.querySelector("#user_bio");
+const user_repostr = document.querySelector("#user_repostr");
+const user_followers = document.querySelector("#user_followers");
+const user_following = document.querySelector("#user_following");
+const user_github_avt = document.querySelector("#github_avt");
+// end the Dom elements!
+theme_change.addEventListener("click", () => {
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  document.documentElement.setAttribute("color-theme", "light");
+  if (theme_change.classList == "light") {
+    theme_change.classList.remove("light");
+    theme_change.classList.add("dark");
+    document.documentElement.classList.add("dark");
+    theme_text.textContent = "LIGHT";
+    theme_icon.src = "../images/svg/sun.svg";
   } else {
-    document.getElementById("body").className =
-      "h-full bg-[#F2F2F2] font-mono bodybgchanlh";
+    theme_change.classList.remove("dark");
+    theme_change.classList.add("light");
+    document.documentElement.classList.remove("dark");
+    theme_text.textContent = "DARK";
+    theme_icon.src = "../images/svg/moon-icon.svg";
   }
-  // end
-  if (
-    document.getElementById("boxbgchange").className ==
-    "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchandk"
-  ) {
-    document.getElementById("boxbgchange").className =
-      "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchanlh";
+});
+serchBtnEl.addEventListener("click", () => {
+  let serchVal = serchInpEl.value;
+  if (serchVal != "" && serchVal != " ") {
+    getTodos(serchVal);
+    async function getTodos(serchval) {
+      try {
+        const res = await fetch(`https://api.github.com/users/${serchval}`);
+        const data = await res.json();
+        user_name.textContent = data.login;
+        user_date_add.textContent = data.created_at;
+        user_bio.textContent = data.bio;
+        user_repostr.textContent = data.public_repos;
+        user_followers.textContent = data.followers;
+        user_following.textContent = data.following;
+        user_github_avt.src = data.avatar_url;
+        serchInpEl.value = "";
+        console.log(data);
+      } catch (error) {
+        alert(error);
+        serchInpEl.value = "";
+      }
+    }
   } else {
-    document.getElementById("boxbgchange").className =
-      "input-form w-full rounded-[15px] p-[9px_10px] flex justify-between items-center shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] bg-[#FEFEFE] boxbgchange boxbgchandk";
-  };
-  // end
-  if (
-    document.getElementById("boxcontentbgchange").className ==
-    "content-box w-full bg-[#FEFEFE] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl"
-  ) {
-    document.getElementById("boxcontentbgchange").className =
-      "content-box w-full bg-[#1E2A47] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl";
-  } else {
-    document.getElementById("boxcontentbgchange").className =
-      "content-box w-full bg-[#FEFEFE] p-12 flex gap-9 shadow-[0px_16px_30px_-10px] shadow-[#4660BB33] rounded-2xl";
-  };
-  // end
-  if (
-    document.getElementById("titleclrchange").className ==
-    "font-bold text-[26px] text-[#222731]"
-  ) {
-    document.getElementById("titleclrchange").className =
-      "font-bold text-[26px] text-[#FFFFFF]";
-  } else {
-    document.getElementById("titleclrchange").className =
-      "font-bold text-[26px] text-[#222731]";
+    alert("Foydalanuvchi nomini kiritng!");
   }
-  // end
-  if (
-    document.getElementById("textclrprimary").className ==
-    "font-bold text-[26px] text-[#222731]"
-  ) {
-    document.getElementById("textclrprimary").className =
-      "font-bold text-[26px] text-[#FFFFFF]";
-  } else {
-    document.getElementById("textclrprimary").className =
-      "font-bold text-[26px] text-[#222731]";
-  }
-  // end
-  if (
-    document.getElementById("conrwbg").className ==
-    "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#F6F8FF]"
-  ) {
-    document.getElementById("conrwbg").className =
-      "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#141D2F]";
-  } else {
-    document.getElementById("conrwbg").className =
-      "result-row flex items-center justify-between w-full rounded-[10px] p-[15px_32px] mt-8 bg-[#F6F8FF]";
-  }
-  // end
-  if (
-     document.getElementById("h2-one").className ==
-    "text-[#2B3442] font-bold text-2xl"
-  ) {
-     document.getElementById("h2-one").className =
-      "text-[#FFFFFF] font-bold text-2xl";
-  } else {
-     document.getElementById("h2-one").className =
-      "text-[#2B3442] font-bold text-2xl";
-  }
-  // end
-  if (
-     document.getElementById("h2-two").className ==
-    "text-[#2B3442] font-bold text-2xl"
-  ) {
-     document.getElementById("h2-two").className =
-      "text-[#FFFFFF] font-bold text-2xl";
-  } else {
-     document.getElementById("h2-two").className =
-      "text-[#2B3442] font-bold text-2xl";
-  }
-  // end
-  if (
-     document.getElementById("h2-three").className ==
-    "text-[#2B3442] font-bold text-2xl"
-  ) {
-     document.getElementById("h2-three").className =
-      "text-[#FFFFFF] font-bold text-2xl";
-  } else {
-     document.getElementById("h2-three").className =
-      "text-[#2B3442] font-bold text-2xl";
-  }
-  // end
-  // document.getElementsByClassName("h2")
-}
+});
